92c3b9671a4a2f6bda4c9da0810801d13d717564
dynamicMosaic <code4nature@gmail.com>
2020-07-31 23:43:26 -0700

    3 category pull

--- bluegrass.js
+++ bluegrass.js
@@ -12,15 +12,15 @@ const puppeteer = require('puppeteer');
     const browser = await puppeteer.launch();
     const [page] = await browser.pages();
 
-    await page.goto('https://www.google.com/search?q=intitle%3Abluegrass+intext%3A+sponsors&oq=intitle%3Abluegrass+intext%3A+sponsors&aqs=chrome..69i57j69i58.14144j0j7&sourceid=chrome&ie=UTF-8');
+    await page.goto('https://www.google.com/search?sxsrf=ALeKk01K7urXdNmUnEWzSTc8dczsCu4hTQ%3A1596256646014&ei=hvEkX_s4pMHuAqrBnYAL&q=%28+bluegrass+OR+americana+OR+folk%29+%22sponsors%22+&oq=%28+bluegrass+OR+americana+OR+folk%29+%22sponsors%22+&gs_lcp=CgZwc3ktYWIQAzIFCCEQqwI6BAgjECc6BQgAEJECOgcIABCxAxBDOgsILhCxAxDHARCjAjoICC4QsQMQgwE6BQgAELEDOggILhDHARCjAjoECAAQQzoKCAAQsQMQsQMQQzoICAAQsQMQgwE6BwgAEBQQhwI6CggAELEDEBQQhwI6DgguELEDEIMBEMcBEKMCOggILhDHARCvAToFCC4QsQM6AggAOg4ILhCxAxCDARDHARCvAToCCC46BggAEBYQHjoECC4QDToICAAQCBANEB46BQghEKABOgUIABDNAjoECCEQClD0qQJY64nHA2DSlccDaApwAHgAgAH0AYgB3zqSAQc0Ny4yNC4zmAEAoAEBqgEHZ3dzLXdpesABAQ&sclient=psy-ab&ved=0ahUKEwj7k-yyl_nqAhWkoFsKHapgB7AQ4dUDCAw&uact=5');
 
-    // way 1
-    const hrefs1 = await page.evaluate(
-      () => Array.from(
-        document.querySelectorAll('a[href]'),
-        a => a.getAttribute('href')
-      )
-    );
+    // // way 1
+    // const hrefs1 = await page.evaluate(
+    //   () => Array.from(
+    //     document.querySelectorAll('div > .r > a[href]'),
+    //     a => a.getAttribute('href')
+    //   )
+    // );
 
     // way 2
     const elementHandles = await page.$$('a');
@@ -31,7 +31,8 @@ const puppeteer = require('puppeteer');
       propertyJsHandles.map(handle => handle.jsonValue())
     );
 
-    console.log(hrefs1, hrefs2);
+    // console.log("---------------------------------- \n ---------------------------------- " + hrefs1 + "---------------------------------- \n ---------------------------------- ");
+    console.log(hrefs2);
 
     await browser.close();
   } catch (err) {
